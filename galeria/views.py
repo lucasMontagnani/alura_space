d9eadf4ac03b9e3ab2aa4dd420a6672b0f9b1986
lucasMontagnani <lucascalilelias@yahoo.com.br>
2023-06-04 19:56:43 -0300

    Lidando com dados

--- galeria/views.py
+++ galeria/views.py
@@ -7,7 +7,15 @@ def index(request):
     return HttpResponse('<h1>Alura Space</h1> <p>Teste2</p>')
 '''
 def index(request):
-    return render(request, 'galeria/index.html')
+
+    dados = {
+        1: {"nome":"Nebulosa de Carina",
+            "legenda":"webbtelescope.org / NASA / James Webb"},
+        2: {"nome":"Galáxia NGC 1079",
+            "legenda":"nasa.org / NASA / Hubble"}
+    }
+
+    return render(request, 'galeria/index.html', {"cards": dados})
 
 def imagem(request):
     return render(request, 'galeria/imagem.html')
